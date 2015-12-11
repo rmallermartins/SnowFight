24e5e282c090d139ebb62904be2351877d58551d
rmallermartins <rmallermartins@gmail.com>
2015-12-10 23:02:09 -0200

    Added Input Class

---
+++ SnowFight/IInput.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SnowFight
+{
+    public interface IInput
+    {
+        KeyboardState LastState { get; set; }
+        KeyboardState ActualState { get; set; }
+
+        void Update(GameTime gameTime);
+        bool Press(Keys key);
+    }
+}
---
+++ SnowFight/Input.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace SnowFight
+{
+    public class Input : IInput
+    {
+        public KeyboardState ActualState { get; set; }
+        public KeyboardState LastState { get; set; }
+
+        public void Update(GameTime gameTime)
+        {
+            LastState = ActualState;
+            ActualState = Keyboard.GetState();
+        }
+
+        public bool Press(Keys key)
+        {
+            return ActualState.IsKeyDown(key);
+        }
+    }
+}
